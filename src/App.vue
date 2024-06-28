d660be0349fb790a6f86eaae78f8f08fa7704dc4
Isaiah <64103005+RuralAnemone@users.noreply.github.com>
2024-06-28 18:44:12 -0500

    step 3

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,15 @@
 <script setup>
-import { reactive, ref } from 'vue'
+import { ref } from 'vue'
 
-const message = ref("hello world");
-console.log(message);
-// message.value = "hello mars";
-
-const counter = reactive({
-  count: 0
-});
-
-counter.count++;
-
+const titleClass = ref('title')
 </script>
 
 <template>
-  <h1>{{ message }}</h1>
-  <p>count is {{ counter.count }}</p>
+  <h1 :class="titleClass">Make me red</h1>
 </template>
+
+<style>
+.title {
+  color: red;
+}
+</style>
