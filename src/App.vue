6743a9cf7292c1ccf6daf598d377d8db0fae5f02
Isaiah <64103005+RuralAnemone@users.noreply.github.com>
2024-06-28 18:51:26 -0500

    start step 4

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,10 @@
 <script setup>
 import { ref } from 'vue'
 
-const titleClass = ref('title')
+const count = ref(0)
 </script>
 
 <template>
-  <h1 :class="titleClass">Make me red</h1>
+  <!-- make this button work -->
+  <button>Count is: {{ count }}</button>
 </template>
-
-<style>
-.title {
-  color: red;
-}
-</style>
