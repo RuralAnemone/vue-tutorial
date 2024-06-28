424b79156e5d4fca21e1da6496b5a8bd4f020612
Isaiah <64103005+RuralAnemone@users.noreply.github.com>
2024-06-28 18:39:19 -0500

    nice

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,19 @@
 <script setup>
-import { ref } from 'vue'
+import { reactive, ref } from 'vue'
 
-const message = ref("hello world")
-console.log(message)
-// message.value = "hello mars"
+const message = ref("hello world");
+console.log(message);
+// message.value = "hello mars";
+
+const counter = reactive({
+  count: 0
+});
+
+counter.count++;
+
 </script>
 
 <template>
   <h1>{{ message }}</h1>
+  <p>count is {{ counter.count }}</p>
 </template>
